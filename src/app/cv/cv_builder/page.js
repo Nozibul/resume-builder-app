f940f0cb4b1f2b6bc88fa93492356876a515464d
Nozibul <nozibulislamspi@gmail.com>
2024-01-07 18:28:12 +0600

    Fixed the cv builder issue

--- src/app/cv/cv_builder/page.js
+++ src/app/cv/cv_builder/page.js
@@ -2,14 +2,35 @@
 import PersonalInfo from "@/components/modules/cv_resune_builder/personalInfo/PersonalInfo"
 import { useSearchParams } from "next/navigation"
 // import { CV_DATA } from "../../../../local-json/CvResumeList"
-// import { CV_RESUME_MENU_LIST } from "../../../../local-json/menuListCv"
+import { CV_RESUME_MENU_LIST } from "../../../../local-json/menuListCv"
 // import { AiOutlineCheck } from "react-icons/ai";
 
 
 const CvBuilder = () => {
  const searchParams = useSearchParams();
- const id = searchParams.get('id')
- console.log(id);
+ const id = searchParams.get('id');
+
+ // define state to detect current active menu-id
+ const [activeMenu, setActiveMenu] = useState(0);
+ const [visible, setVisible] = useState(false);
+
+
+ //  useEffect(() => {
+ //   lastAttemptIndex && setActiveMenuItem(lastAttemptIndex);
+ // }, [lastAttemptIndex]);
+
+ // this method help us to detect is user submit form successfully, or cancel
+  const isCompletedTask = (status = "cancel") => {
+    if (activeMenu < 0 || activeMenu > 6) return null;
+
+    if (status === "back") setActiveMenu((prv) => prv - 1);
+    else if (status === "next") setActiveMenu((prv) => prv + 1);
+    if (process.env.NODE_ENV === "development") console.log(status, "status");
+
+    // scrollToTopMethod();
+  };
+
+
   return (
     <div>
      <p>CV Builder -- { id }</p>
